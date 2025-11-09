41a3ece067bae451e9a26ecd37b1008c77688d0a
Yash <yashvloggs20@gmail.com>
2025-11-09 17:02:08 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical chatbot",
+    version="0.1.0",
+    author="Yash Singh",
+    author_email="yashvloggs20@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
